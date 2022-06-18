574677f83691284b2bfc84fa12d6e6522536e3e4
Hofarkas <szabo.ida97@gmail.com>
2022-06-18 20:46:23 +0200

    new data added

--- test_conduit/article_data.py
+++ test_conduit/article_data.py
@@ -4,3 +4,7 @@ article = {
     'main': 'Valószerűtlennek tűnő, parányi élőlényt örökített meg Andreas Kay tudós az ecuadori esőerdő mélyén, ahol a világ talán legkülönösebb kinézetű ízeltlábúja rejtőzik. A Metagryne bicolumnata néven ismert kaszáspókféle meglehetősen aprócska, ugyanakkor igencsak feltűnő jelenség, hiszen feje (pontosabban előteste) leginkább egy kutyára emlékeztet.',
     'tag': 'állat'
 }
+
+edit_article = {
+    'new_title': 'Kutyapók az erdő mélyén'
+}
