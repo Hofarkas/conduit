0ad6c0e702d25fc24b334704597b0bdfce0a4500
Hofarkas <szabo.ida97@gmail.com>
2022-06-23 16:25:45 +0200

    final changes

--- test_conduit/vizsgaremek.py
+++ test_conduit/vizsgaremek.py
@@ -1,7 +1,6 @@
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
